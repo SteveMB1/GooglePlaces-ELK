be2c780052205d817f6fd48454872ea6669a813b
Steven <SteveMB1@users.noreply.github.com>
2018-02-20 23:24:39 +0000

    adding for loop and creating external document for variables

--- Google_Grab_ELK-Push.js
+++ Google_Grab_ELK-Push.js
@@ -1,4 +1,5 @@
 const util = require('util'),
+config = require('./config.json');
 async = require('async'),
 busy_hours = require('busy-hours'),
 elasticsearch = require('elasticsearch'),
@@ -7,35 +8,32 @@ ES = new elasticsearch.Client({
   log: 'error'
 });
 
-GOOGLE_API_KEY = ''
-GOOGLE LOCATION_ID = ''
-ELK_INDEX_NAME = ''
-
 //Get the weekday as a number (0-6)
 var d = new Date();
 dayArr = d.getDay();
 
-const toES = async () => {
-	await	busy_hours(GOOGLE_LOCATION_ID, GOOGLE_API_KEY).then(data => {
-	ES.count({ index: ELK_INDEX_NAME }, function (count_error, count_response) {
-		if ( count_response.error ) {
-			id_numb = 1;
-			} else {
-				id_numb = (count_response.count) + (1);
-		};
-	ES.create({
-		index: 'competition',
-		type: 'string',
-		id: id_numb,
-		body: {
-			title: 'Comp',
-			published: true,
-			day: data.week[dayArr].day,
-			hours: data.week[dayArr].hours,
-			'@timestamp': new Date().toISOString()
-		}
-		}, function (error, response) { console.log( "" + util.inspect(response,error, {showHidden: false, depth: null}) + "" ) });
-	});
-	});
-};
-toES()
+async.forEach(config.LOCATIONS,function(err,res) {
+	const toES = async () => {
+		await	busy_hours( config.LOCATIONS[0].gid, config.GOOGLE_API_KEY ).then(data => {
+		ES.count({ index: config.ELK_INDEX_NAME }, function (count_error, count_response) {
+			if ( count_response.error ) {
+				id_numb = 1;
+				} else {
+					id_numb = (count_response.count) + (1);
+			};
+		ES.create({
+			index: config.ELK_INDEX_NAME,
+			type: 'string',
+			id: id_numb,
+			body: {
+				title: config.LOCATIONS[0].title,
+				day: data.week[dayArr].day,
+				hours: data.week[dayArr].hours,
+				'@timestamp': new Date().toISOString()
+			}
+			}, function (error, response) { console.log( "" + util.inspect(response,error, {showHidden: false, depth: null}) + "" ) });
+		});
+		});
+	};
+	toES()
+});
