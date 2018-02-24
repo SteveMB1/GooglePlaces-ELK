932836e58c9a4d473be32a5605080959ca196ee3
Steven <SteveMB1@users.noreply.github.com>
2018-02-24 06:39:12 +0000

    WIP async, corrected object ref.

--- Google_Grab_ELK-Push.js
+++ Google_Grab_ELK-Push.js
@@ -3,46 +3,42 @@ const util = require('util'),
   async = require('async'),
   busy_hours = require('busy-hours'),
   elasticsearch = require('elasticsearch'),
-  ES = new elasticsearch.Client({
-    host: '127.0.0.1:9200',
-    log: 'error'
-  });
+  ES = new elasticsearch.Client(config.elasticsearch);
 
 //day-of-week as integer (0-6)
 const d = new Date(),
   dayArr = d.getDay();
 
 function uploadKibana(count) {
-  async.each(config.LOCATIONS, function(data, err) {
-    var toES = async () => {
-      await busy_hours(data.gid, config.GOOGLE_API_KEY).then(data => {
-      console.log('BIG COUNT: '+count);
-        ES.create({
-            index: config.ELK_INDEX_NAME,
-            type: 'string',
-            id: count,
-            body: {
-              title: data.title,
-              day: data.week[dayArr].day,
-              hours: data.week[dayArr].hours,
-              '@timestamp': new Date().toISOString()
-            }
-          },
-          function(error, response) {
-            console.log("" + util.inspect(response, error, {
-              showHidden: false,
-              depth: null
-            }) + "");
-            count++;
-            console.log("Elasticsearch ID Count: " + count);
-          });
-      });
-    };
-    toES();
+  count++
+  async.each(config.LOCATIONS, function(locations, err) {
+    busy_hours(locations.gid, config.GOOGLE_API_KEY).then(data => {
+      console.log("Elasticsearch ID Count: " + count);
+      ES.create({
+          index: config.ELK_INDEX_NAME,
+          type: 'string',
+          id: count,
+          body: {
+            title: locations.title,
+            day: data.week[dayArr].day,
+            hours: data.week[dayArr].hours,
+            '@timestamp': new Date().toISOString()
+          }
+        },
+        function(error, response) {
+          console.log("" + util.inspect(response, error, {
+            showHidden: false,
+            depth: null
+          }) + "");
+          count++;
+        });
+    });
   });
 };
 
-ES.count({ index: config.ELK_INDEX_NAME }, function(count_error, count_response) {
+ES.count({
+  index: config.ELK_INDEX_NAME
+}, function(count_error, count_response) {
   console.log("Data from count: " + util.inspect(count_response, count_error, {
     showHidden: false,
     depth: null
@@ -50,7 +46,7 @@ ES.count({ index: config.ELK_INDEX_NAME }, function(count_error, count_response)
   if (count_response.error) {
     var id_numb = 2;
   } else {
-    var id_numb = (count_response.count) + (1);
+    var id_numb = count_response.count;
   };
   console.log("Within function: " + id_numb)
   uploadKibana(id_numb)
