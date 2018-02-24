70b42a76057d951e5c8a9735e468749e42a9c19f
Steven <SteveMB1@users.noreply.github.com>
2018-02-24 06:07:25 +0000

    working on ID count and async await

--- Google_Grab_ELK-Push.js
+++ Google_Grab_ELK-Push.js
@@ -1,45 +1,57 @@
 const util = require('util'),
-config = require('./config.json');
-async = require('async'),
-busy_hours = require('busy-hours'),
-elasticsearch = require('elasticsearch'),
-ES = new elasticsearch.Client({
-  host: '127.0.0.1:9200',
-  log: 'error'
-});
+  config = require('./config.json'),
+  async = require('async'),
+  busy_hours = require('busy-hours'),
+  elasticsearch = require('elasticsearch'),
+  ES = new elasticsearch.Client({
+    host: '127.0.0.1:9200',
+    log: 'error'
+  });
 
-//Get the weekday as a number (0-6)
-var d = new Date();
-dayArr = d.getDay();
+//day-of-week as integer (0-6)
+const d = new Date(),
+  dayArr = d.getDay();
 
-ES.count({ index: config.ELK_INDEX_NAME }, function (count_error, count_response) {
-	if ( count_response.error ) {
-		id_numb = 1;
-		} else {
-			id_numb = (count_response.count) + (1);
-	};
-		});
+function uploadKibana(count) {
+  async.each(config.LOCATIONS, function(data, err) {
+    var toES = async () => {
+      await busy_hours(data.gid, config.GOOGLE_API_KEY).then(data => {
+      console.log('BIG COUNT: '+count);
+        ES.create({
+            index: config.ELK_INDEX_NAME,
+            type: 'string',
+            id: count,
+            body: {
+              title: data.title,
+              day: data.week[dayArr].day,
+              hours: data.week[dayArr].hours,
+              '@timestamp': new Date().toISOString()
+            }
+          },
+          function(error, response) {
+            console.log("" + util.inspect(response, error, {
+              showHidden: false,
+              depth: null
+            }) + "");
+            count++;
+            console.log("Elasticsearch ID Count: " + count);
+          });
+      });
+    };
+    toES();
+  });
+};
 
-async.each(config.LOCATIONS,function(data,err) {
-console.log(data.gid)
-	const toES = async () => {
-		await	busy_hours( data.gid, config.GOOGLE_API_KEY ).then(data => {
-		ES.create({
-			index: config.ELK_INDEX_NAME,
-			type: 'string',
-			id: id_numb,
-			body: {
-				title: data.title,
-				day: data.week[dayArr].day,
-				hours: data.week[dayArr].hours,
-				'@timestamp': new Date().toISOString()
-			}
-			}, 
-			function (error, response) { console.log( "" + util.inspect(response,error, {showHidden: false, depth: null}) + "" );
-				id_numb++;
-				console.log(id_numb); 
-			});
-		});
-	};
-	toES();
+ES.count({ index: config.ELK_INDEX_NAME }, function(count_error, count_response) {
+  console.log("Data from count: " + util.inspect(count_response, count_error, {
+    showHidden: false,
+    depth: null
+  }) + "");
+  if (count_response.error) {
+    var id_numb = 2;
+  } else {
+    var id_numb = (count_response.count) + (1);
+  };
+  console.log("Within function: " + id_numb)
+  uploadKibana(id_numb)
 });
